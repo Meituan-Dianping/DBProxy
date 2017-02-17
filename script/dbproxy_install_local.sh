e4d6afbc4e194623b49b61947ba8b6bc30afa2d8
tsthght <781181214@qq.com>
2017-02-17 16:17:08 +0800

    fix path error in dbproxy_install_local.sh

--- script/dbproxy_install_local.sh
+++ script/dbproxy_install_local.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-basedir=$(pwd)
+basedir=$(cd `dirname $0`; pwd)/../
 mkdir -p "/opt/tmp/dbproxy_log/"
 
 ####yum install 依赖包
 yum install -y libevent-devel lua-devel openssl-devel flex Percona-Server-devel-55.x86_64 Percona-Server-client-55.x86_64 jemalloc jemalloc-devel glib2 glib2-devel
 
 ### install dbproxy
-cd $basedir/dbproxy
+cd $basedir
 ./configure --prefix=/usr/local/mysql-proxy CFLAGS="-g -O0" && make -j 4 && make install
 
 ###mv source.cnf 
 cd $basedir
 mkdir -p /usr/local/mysql-proxy/conf
-mv ./dbproxy/script/source.cnf.samples /usr/local/mysql-proxy/conf/source.cnf
+cp ./script/source.cnf.samples /usr/local/mysql-proxy/conf/source.cnf
